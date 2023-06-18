c41a6b0ba5f90d08529663595e69499ea032fd02
Victor <maragiogliov@protonmail.com>
2023-06-19 00:29:48 +0200

    go

--- src/content/ResultsPage/ResultsPage.js
+++ src/content/ResultsPage/ResultsPage.js
@@ -1,22 +1,33 @@
-import React from "react";
-import "./_results-page.scss";
-import "@carbon/charts/styles.css";
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import './_results-page.scss';
 
-const ResultsPage = () => {
-
-
+const DataTable = () => {
   return (
-    <>
-      <div className="results-container">
-   
-         
-              Results
-       
-       
-      </div>
-    </>
+    <div className="results-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Categories</th>
+            <th>Results in Kg of CO2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr><td>Household</td><td>Data 2</td></tr>
+          <tr><td>Data 3</td><td>Data 4</td></tr>
+          <tr><td>Data 5</td><td>Data 6</td></tr>
+          <tr><td>Data 7</td><td>Data 8</td></tr>
+          <tr><td>Data 9</td><td>Data 10</td></tr>
+          <tr><td>Data 11</td><td>Data 12</td></tr>
+          <tr><td>Data 13</td><td>Data 14</td></tr>
+          <tr><td>Data 15</td><td>Data 16</td></tr>
+          <tr><td>Data 17</td><td>Data 18</td></tr>
+          <tr><td>Data 19</td><td>Data 20</td></tr>
+          <tr><td>Data 21</td><td>Data 22</td></tr>
+          <tr><td>Data 23</td><td>Data 24</td></tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-export default withRouter(ResultsPage);
+export default DataTable;
