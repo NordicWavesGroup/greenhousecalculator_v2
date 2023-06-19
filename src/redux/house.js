13135dad19f2789d3aa6d3b59bbe3ab3bdd9e456
Victor <maragiogliov@protonmail.com>
2023-06-19 18:28:57 +0200

    GO

--- src/redux/house.js
+++ src/redux/house.js
@@ -12,20 +12,21 @@ export const houseSlice = createSlice({
   initialState,
   reducers: {
   
-    //Electricity Global State
+    
     setElectricityAverage: (state, action) => {
       state.electricity = action.payload;
     },
  
     setElectricityFootprint: (state, action) => {
       state.electricityFootprint = action.payload;
-    }
+    },
  
+    setCO2Result: (state, action) => {
+    state.co2Result = action.payload;
+  }
   }
 });
-export const setCO2Result = (state, action) => {
-  state.co2Result = action.payload;
-};
+
 
 export const { 
                selectCountry, 
