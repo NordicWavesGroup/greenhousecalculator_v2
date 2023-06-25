b2a73754ba1518b0447c86a76a3a668af8f634f8
Victor <maragiogliov@protonmail.com>
2023-06-25 19:59:32 +0200

    working

--- src/content/HousePage/House_Electricity.js
+++ src/content/HousePage/House_Electricity.js
@@ -1,64 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import {  setkWh_CO2Result} from '../../redux/house';
-
+import { setkWh, setkWh_CO2Result } from '../../redux/house';
 import { NumberInput } from 'carbon-components-react';
-
 import { Slider } from '@carbon/react';
 
 const HouseElectricity = () => {
-  const { kWh, kWh_factor, kWh_CO2Result} = useSelector((state) => state.house);
-
+  const { kWh, kWh_factor, kWh_CO2Result } = useSelector((state) => state.house);
   const dispatch = useDispatch();
 
-
-  const handleSliderChange = (event) => {
-    const newValue = event.value;
+  const handleValueChange = (newValue) => {
+    const newCO2Result = newValue * kWh_factor;
   
-    // Calculate the CO2 result based on the slider value
-    const kWh_CO2Result = newValue * kWh_factor; // Replace 4 with the actual factor value
-  
-    // Dispatch the CO2 result to the Redux store
-    dispatch(setkWh_CO2Result (kWh_CO2Result));
-
-    // Update the CO2 result state
-    setkWh_CO2Result(kWh_CO2Result);
+    dispatch(setkWh(newValue));
+    dispatch(setkWh_CO2Result(newCO2Result));
   };
-  
 
   return (
-    <>
-      <div className="input-group">
-        <Slider
-          id="electricity-slider"
-          labelText="Electricity Consumption"
-          className="house-block-middle-form"
-          min={0}
-          max={9999}
-          step={1}
-          value={kWh}
-          onChange={handleSliderChange}
-        />
-
-        
-    
-      </div>
-        <NumberInput
-              helperText=""
-              id="tj-input"
-              invalidText=""
-              label="Custom"
-              max={9999}
-              min={0}
-              step={10}
-              value={kWh}
-              size={'md'}
-              onChange={handleSliderChange}
-
-        />
-  
+    <div className="input-group">
+      <Slider
+        id="electricity-slider"
+        labelText="Electricity Consumption"
+        className="house-block-middle-form"
+        min={0}
+        max={9999}
+        step={1}
+        value={kWh}
+        onChange={({value}) => handleValueChange(value)}
+      />
+      <NumberInput
+        helperText=""
+        id="tj-input"
+        invalidText=""
+        label="Custom"
+        max={9999}
+        min={0}
+        step={10}
+        value={kWh}
+        size={'md'}
+        className="no-buttons"
+        onChange={({target: {value}}) => handleValueChange(Number(value))}
+      />
       <p>kWh Results in CO2: {kWh_CO2Result} Kg</p>
-    </>
+    </div>
   );
 };
 
--- src/redux/house.js
+++ src/redux/house.js
@@ -1,44 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-
   kWh_CO2Result: 0,
-  kWh_factor:0.50,
+  kWh_factor: 0.50,
   kWh: 1500,
-  
-
 };
 
 export const houseSlice = createSlice({
   name: "house",
   initialState,
   reducers: {
- 
     setkWh_CO2Result: (state, action) => {
       state.kWh_CO2Result = action.payload;
     },
-
     setkWh: (state, action) => {
       state.kWh = action.payload;
     },
-    
-    
     setkWh_factor: (state, action) => {
       state.kWh_factor = action.payload;
     },
-    
-  
   }
 });
 
 export const { 
-              
-              kWh_CO2Result,
-         
-              kWh,
-              kWh_factor,
-              setkWh_CO2Result
-
-              } = houseSlice.actions;
+  setkWh_CO2Result,
+  setkWh,
+  setkWh_factor
+} = houseSlice.actions;
 
 export default houseSlice.reducer;
