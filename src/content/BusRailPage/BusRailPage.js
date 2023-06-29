f9e98c00717ca39ad45e2e9c65a7ba655f7012e2
Victor <maragiogliov@protonmail.com>
2023-06-29 22:30:36 +0200

    go

--- src/content/BusRailPage/BusRailPage.js
+++ src/content/BusRailPage/BusRailPage.js
@@ -3,17 +3,14 @@ import "./_bus-rail-page.scss";
 import IconsNavigation from "../IconsNavigation/IconsNavigation";
 import MySwipeableTabs from "../BusRailPage/MySwipeableTabs";
 import Waves from "../Waves";
-import ResultTotalResults from "../../content/ResultsPage/Results_TotalResults"
 
 const BusRailPage = () => {
 
   return (
     <>
-        <div className="global-total-result-fixed">
-      <ResultTotalResults />
-</div>
+
       <IconsNavigation />
-      <div className="household_sec">
+     
         <div className="container">
           <div className="household_row">
             <div className="housHold_col">
@@ -25,7 +22,7 @@ const BusRailPage = () => {
             </div>
           </div>
         </div>
-      </div>
+
       <Waves />
     </>
   );
